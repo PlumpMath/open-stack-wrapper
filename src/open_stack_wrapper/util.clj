df8c72cd34c48ae03ed69cfda14efdf73b0ef2dd
tangrammer <juanantonioruz@gmail.com>
2014-01-30 00:13:59 +0100

    refactor pprint-json-scheme

--- src/open_stack_wrapper/util.clj
+++ src/open_stack_wrapper/util.clj
@@ -4,51 +4,42 @@
             [clojure.data.json :as json])
   )
 
-
-(def example {:key1 1
-              :key2 {:key3 "hola" :key4 {}}
-              :key5 [5 6 [7 [8] {:key6 [{:key7 17}]}]]
-              })
-
-(defn keywordize [v]
-  (let [s (.getSimpleName (type v))
-        t (keyword (clojure.string/lower-case s))
+(defn pprint-json-scheme
+  "this fn analyze the json structure thinking that in vectors the elements repeat the same scheme,
+   that's to say the elements of the vectors are of the same 'class' from an OO point of view.
+   But not the same prediction on maps where each element have his identity(class) on the map, that's the point of the dictionary elements they are (class)    definitions
+"
+  [the-json-data]
+  (let [keywordize (fn [v]
+                     (let [s (.getSimpleName (type v))
+                           t (keyword (clojure.string/lower-case s))]
+                       (condp = t
+                         :long "N"
+                         :string "S"
+                         :persistentarraymap "{}"
+                         t)))
+        deep-read (fn deep-read [jsondata]
+             (cond
+              (map? jsondata) (into {} (vec
+                                  (map (fn [[json-key json-value]]
+                                         (cond
+                                          (map? json-value) (if (empty? json-value) [json-key (keywordize {})] [json-key   (into {} (deep-read json-value))])
+                                          (vector? json-value) (if (empty? json-value) [json-key []] [json-key [(deep-read (first json-value))]])
+                                          :else [json-key (keywordize json-value)])) jsondata)))
+              (vector? jsondata)[(deep-read (first jsondata))]
+              :else (keywordize jsondata)))
+        result  (into {}  (deep-read the-json-data))
         ]
-
-         (condp = t
-           :long "N"
-           :string "S"
-           :persistentarraymap "{}"
-           t)
-    ))
-
-(defn pprint-json-scheme [the-json-data]
-  (let [result  ((fn rr [ex]
-          (if (map? ex)
-            (into {} (vec (map (fn [[k v]]  (if (map?  v)
-                                             (if (empty? v)
-                                               [k (keywordize {})]
-                                               [k   (into {} (rr v))]
-                                               )
-                                             (if (vector? v)
-                                               (if (empty? v)
-                                                 [k []]
-                                                 [k  [(rr (first v))]]
-                                                 )
-
-                                               [k  (keywordize v)]
-                                               ))) ex)))
-            (if (vector? ex)
-              [(rr (first ex))]
-              (keywordize ex)
-              )
-
-
-            ) ) the-json-data)
-        r (into {} result)
-        ]
-    (pprint r)
-    r
+    (pprint result)
+    result
     )
 
-    )
+  )
+
+(comment
+  (let [example {:key1 1
+                  :key2 {:key3 "hola" :key4 {}}
+                  :key5 [[{:key6 [{:key7 1}]}]]
+                 }]
+    (pprint-json-scheme example))
+  )
