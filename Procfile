4a551123453157c44bda047792594b90a2f1d513
tangrammer <juanantonioruz@gmail.com>
2014-01-30 18:12:25 +0100

    heroku config

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/open-stack-wrapper-standalone.jar clojure.main -m open-stack-wrapper.rest $PORT
--- project.clj
+++ project.clj
@@ -9,4 +9,7 @@
                  [org.clojure/data.json "0.2.4"]
                  [compojure "1.1.3"]
 		 [ring/ring-core "1.2.1"]
-                 [ring/ring-jetty-adapter "1.1.0"]])
+                 [ring/ring-jetty-adapter "1.1.0"]]
+  :uberjar-name "open-stack-wrapper-standalone.jar"
+  :min-lein-version "2.0.0"
+  )
