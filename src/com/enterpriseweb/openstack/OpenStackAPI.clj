d18f54ad796f238b75340e7861c458d6fa0313a3
tangrammer <tangrammer@tangrammers-Mac-mini.local>
2014-03-04 09:48:32 +0100

    load-properties

--- src/com/enterpriseweb/openstack/OpenStackAPI.clj
+++ src/com/enterpriseweb/openstack/OpenStackAPI.clj
@@ -1,12 +1,16 @@
 (ns com.enterpriseweb.openstack.OpenStackAPI
   #_(:use [ew_snmp.v2 :only (v2-json)]
         [com.enterpriseweb.snmp.utils])
-  (:require [clojure.data.json :as clj-json])
+  (:require [clojure.data.json :as clj-json][clojure.java.io :as io])
   (:import [org.json JSONObject])
   (:gen-class :methods
               [#^{:static true} [json [org.json.JSONObject] org.json.JSONObject]])
   )
 
+
+(defn load-config [filename]
+  (with-open [r (io/reader filename)]
+    (read (java.io.PushbackReader. r))))
 
 (defn create-json-java-object [clojure-json-object]
   (JSONObject. (str clojure-json-object)))
